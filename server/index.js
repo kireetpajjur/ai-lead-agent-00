6545d6fff6eac2a88e04556b6e34f9971d017995
kireetpajjur <krpajjur@gmail.com>
2025-05-28 18:21:57 -0500

    Define root route to handle GET /

--- server/index.js
+++ server/index.js
@@ -8,5 +8,10 @@ const app = express();
 app.use(express.json());
 app.use('/api/leads', leadRoutes);
 
+app.get('/', (req, res) => {
+    res.send("Welcome to the AI Lead Agent!.");
+});
+
+module.exports = app;
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
